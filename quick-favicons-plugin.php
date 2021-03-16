17a47cf63c524b6aeafa6841897d91c6a7a3f8a2
Antti Pohjola <summeli@summeli.com>
2021-03-16 14:05:33 +0200

    init

---
+++ quick-favicons-plugin.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Quick Favicon Plugin
+Description: Adding favicon into your wordpress site. This plugin is best used with https://www.favicon-generator.org/
+*/
+
+function add_my_favicon() {
+    $favicon_path = plugins_url( '/favicons/', __FILE__ );    
+
+   // echo '<link rel="shortcut icon" href="' . $favicon_path . '" />';
+    echo '<link rel="apple-touch-icon" sizes="57x57" href="' . $favicon_path . 'apple-icon-57x57.png">';
+    echo '<link rel="apple-touch-icon" sizes="60x60" href="' . $favicon_path . 'apple-icon-60x60.png">';
+    echo '<link rel="apple-touch-icon" sizes="72x72" href="' . $favicon_path . 'apple-icon-72x72.png">';
+    echo '<link rel="apple-touch-icon" sizes="76x76" href="' . $favicon_path . 'apple-icon-76x76.png">';
+    echo '<link rel="apple-touch-icon" sizes="114x114" href="' . $favicon_path . 'apple-icon-114x114.png">';
+    echo '<link rel="apple-touch-icon" sizes="120x120" href="' . $favicon_path . 'apple-icon-120x120.png">';
+    echo '<link rel="apple-touch-icon" sizes="144x144" href="' . $favicon_path . 'apple-icon-144x144.png">';
+    echo '<link rel="apple-touch-icon" sizes="152x152" href="' . $favicon_path . 'apple-icon-152x152.png">';
+    echo '<link rel="apple-touch-icon" sizes="180x180" href="' . $favicon_path . 'apple-icon-180x180.png">';
+    echo '<link rel="icon" type="image/png" sizes="192x192"  href="' . $favicon_path . 'android-icon-192x192.png">';
+    echo '<link rel="icon" type="image/png" sizes="32x32" href="' . $favicon_path . 'favicon-32x32.png">';
+    echo '<link rel="icon" type="image/png" sizes="96x96" href="' . $favicon_path . 'favicon-96x96.png">';
+    echo '<link rel="icon" type="image/png" sizes="16x16" href="' . $favicon_path . 'favicon-16x16.png">';
+    echo '<link rel="manifest" href="' . $favicon_path . 'manifest.json">';
+}
+
+
+add_action( 'wp_head', 'add_my_favicon' ); //front end
+add_action( 'admin_head', 'add_my_favicon' ); //admin end
+?>
+
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Admin Console Favicon ===
+Contributors: summeli
+Tags: favicon
+Requires at least: 4.6^M
+Tested up to: 4.9.2
+Requires PHP: 5.3^M
+Stable tag: 1.0^M
+License: GPLv2 or later^M
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== DESCRIPTION ==
+
+This is best used with online favicon generatior at https://www.favicon-generator.org/
